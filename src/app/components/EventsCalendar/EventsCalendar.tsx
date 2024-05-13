985ab63635ed5c3a604cbc54c61ec04b27e370ea
LaptopNickolas <panteleew.kolya@gmail.com>
2024-05-13 22:09:27 +0900

    Сверстал календарь мероприятий

--- src/app/components/EventsCalendar/EventsCalendar.tsx
+++ src/app/components/EventsCalendar/EventsCalendar.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 import './EventsCalendar.css';
+import { UseCalendar } from '../../hooks/UseCalendar';
+import { ArrowButton } from '../../../UI/ArrowButton/ArrowButton';
 
 export function EventsCalendar() {
+
+    const { currentDate, setcurrentDate, prevDays, setPrevDays, nextDays, setNextDays, days, prevDaysClick, nextDaysClick } = UseCalendar();
+
+
   return (<section className='events-calendar'>
-    Мероприятия
+    <h3 className='events-calendar__title'>Календарь мероприятий Июнь, 2024</h3>
+   
+    <ul className='events-calendar__date-list list-style'>
+
+        <ArrowButton direction='left' changeDate={prevDaysClick}/>
+
+        {days.map(day => ( <li key={day} className='events-calendar__day'>
+            {day}
+        </li>
+        ))}
+
+        <ArrowButton direction='right' changeDate={nextDaysClick}/>
+    </ul>
   </section>)
 }
