87ce0ac8205015c9adef4b1b3aa00f9dd92f4396
LaptopNickolas <panteleew.kolya@gmail.com>
2024-05-27 15:55:23 +0900

    Доделал страницу "направления"

--- src/UI/Button/Button.tsx
+++ src/UI/Button/Button.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { ButtonProps } from "./ButtonProps";
 import './Button.css';
 
-export function Button({ children }: ButtonProps) {
+export function Button({ children, type }: ButtonProps) {
+
+    const orangeBorder = type === 'orangeBorder' ? 'button_type_orange-border': '';
 
     return (
-        <button className="button">
+        <button className={`button ${orangeBorder}`}>
             {children}
       </button>
     );
